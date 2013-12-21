ceae71779e285b1b992b36b0561e39525b7e9a49
Alberto96 <albertoguanti@gmail.com>
2013-12-21 17:47:59 +0100

    EFS: add backup to SdCard

--- ics-ramdisk/cwm/updater.sh
+++ ics-ramdisk/cwm/updater.sh
@@ -7,6 +7,10 @@ busybox umount /system
 busybox mkdir /efs
 busybox mount -t yaffs2 /dev/block/mtdblock4 /efs
 busybox mount -t yaffs2 /dev/block/mtdblock2 /system
+busybox rm -rf /sdcard/GearKernel/efs
+busybox mkdir -p /sdcard/GearKernel
+busybox mkdir -p /sdcard/GearKernel/efs
+busybox cp -Rf /efs/* /sdcard/GearKernel/efs/
 busybox rm -rf /system/GearKernel/efs
 busybox mkdir -p /system/GearKernel
 busybox mkdir -p /system/GearKernel/efs
